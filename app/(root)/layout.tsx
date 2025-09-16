60f7dc212569bba02b7fb0acb4c7b1a233ad4ce9
code-shams <code.meshams@gmail.com>
2025-09-16 20:33:52 +0600

    Authentication using auth js implemented

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar"
+
+const layout = ({children}: Readonly<{children: React.ReactNode}>) => {
+  return (
+    <div>
+        <Navbar></Navbar>
+        {children}
+    </div>
+  ) 
+}
+
+export default layout
